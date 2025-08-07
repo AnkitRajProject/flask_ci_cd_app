adecb8369bd33e71fcb4c94479bb891c1a65ecd2
265495 <265495@ust.com>
2025-08-07 16:20:58 +0530

    test4

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,4 +5,4 @@ def test_index():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello, CI/CD Flask!" in response.data
+    assert b"Hello, CI/CD Flask.!" in response.data
